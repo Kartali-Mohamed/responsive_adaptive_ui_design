1f7b2805c68e160e85cc219f0ff39e937f9102dc
Kartali-Mohamed <mohameekartali@gmail.com>
2024-03-04 15:00:17 +0100

    add background color to dashboard view

--- lib/interface/views/dashboard_view.dart
+++ lib/interface/views/dashboard_view.dart
@@ -11,6 +11,7 @@ class DashboardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF7F9FA),
       body: AdaptiveLayout(
           mobileLayout: (context) => const DashboardMobileLayout(),
           tableLayout: (context) => const DashboardTabletLayout(),
