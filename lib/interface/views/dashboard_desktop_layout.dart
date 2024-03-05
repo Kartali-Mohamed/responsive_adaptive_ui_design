fbdda07ddf3237cc7e200a6385c7e597a6d8d454
Kartali-Mohamed <mohameekartali@gmail.com>
2024-03-05 16:00:26 +0100

    commente all unused widgets in desktop layout

--- lib/interface/views/dashboard_desktop_layout.dart
+++ lib/interface/views/dashboard_desktop_layout.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_adaptive_ui_design/interface/widgets/all_expenses/all_expenses.dart';
-import 'package:responsive_adaptive_ui_design/interface/widgets/income/income.dart';
-import 'package:responsive_adaptive_ui_design/interface/widgets/my_card/my_card.dart';
-import 'package:responsive_adaptive_ui_design/interface/widgets/quick_invoice/quick_invoice.dart';
+import 'package:responsive_adaptive_ui_design/interface/widgets/all_expenses/custom_all_expenses.dart';
+// import 'package:responsive_adaptive_ui_design/interface/widgets/income/income.dart';
+// import 'package:responsive_adaptive_ui_design/interface/widgets/my_card/my_card.dart';
+// import 'package:responsive_adaptive_ui_design/interface/widgets/quick_invoice/quick_invoice.dart';
 
 import '../widgets/drawer/custom_drawer.dart';
 
@@ -20,25 +20,25 @@ class DashboardDesktopLayout extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(height: 40),
-              Expanded(child: AllExpenses()),
-              SizedBox(height: 24),
-              Expanded(child: QuickInvoice()),
-              SizedBox(height: 32),
+              CustomAllExpenses(),
+              // SizedBox(height: 24),
+              // Expanded(child: QuickInvoice()),
+              // SizedBox(height: 32),
             ],
           ),
         ),
-        SizedBox(width: 24),
-        Expanded(
-            flex: 3,
-            child: Column(
-              children: [
-                SizedBox(height: 40),
-                Expanded(flex: 3, child: MyCard()),
-                SizedBox(height: 24),
-                Expanded(flex: 1, child: Income()),
-              ],
-            )),
-        SizedBox(width: 32),
+        // SizedBox(width: 24),
+        // Expanded(
+        //     flex: 3,
+        //     child: Column(
+        //       children: [
+        //         SizedBox(height: 40),
+        //         Expanded(flex: 3, child: MyCard()),
+        //         SizedBox(height: 24),
+        //         Expanded(flex: 1, child: Income()),
+        //       ],
+        //     )),
+        // SizedBox(width: 32),
       ],
     );
   }
