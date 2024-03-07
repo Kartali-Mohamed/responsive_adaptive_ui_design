bf804e2d2b8bdf7b2a3f6b5b298aae013f6613d8
kartali mohamed <mohameekartali@gmail.com>
2024-03-07 15:43:19 +0100

    create card & transaction & income section

---
+++ lib/interface/widgets/mycard_and_transaction_and_income_section.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'card_and_transaction_section.dart';
+import 'income/income_section.dart';
+
+class MyCardAndTransactionAndIncomeSection extends StatelessWidget {
+  const MyCardAndTransactionAndIncomeSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 40),
+          CardAndTransactionSection(),
+          SizedBox(height: 24),
+          IncomeSection(),
+        ],
+      ),
+    );
+  }
+}
