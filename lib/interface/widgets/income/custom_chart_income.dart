cd954df68b7984908de8b66d5f719086c5454003
kartali mohamed <mohameekartali@gmail.com>
2024-03-07 16:27:21 +0100

    add redius

--- lib/interface/widgets/income/custom_chart_income.dart
+++ lib/interface/widgets/income/custom_chart_income.dart
@@ -12,13 +12,25 @@ class CustomChartIncome extends StatelessWidget {
   PieChartData getChartData() {
     return PieChartData(sectionsSpace: 0, sections: [
       PieChartSectionData(
-          value: 40, color: const Color(0xFF208BC7), showTitle: false),
+          radius: 25,
+          value: 40,
+          color: const Color(0xFF208BC7),
+          showTitle: false),
       PieChartSectionData(
-          value: 25, color: const Color(0xFF4DB7F2), showTitle: false),
+          radius: 25,
+          value: 25,
+          color: const Color(0xFF4DB7F2),
+          showTitle: false),
       PieChartSectionData(
-          value: 20, color: const Color(0xFF064060), showTitle: false),
+          radius: 25,
+          value: 20,
+          color: const Color(0xFF064060),
+          showTitle: false),
       PieChartSectionData(
-          value: 22, color: const Color(0xFFE2DECD), showTitle: false),
+          radius: 25,
+          value: 22,
+          color: const Color(0xFFE2DECD),
+          showTitle: false),
     ]);
   }
 }
