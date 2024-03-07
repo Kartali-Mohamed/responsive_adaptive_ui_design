96ab3c33e15ab44bb64071795c3bdcc53464569d
kartali mohamed <mohameekartali@gmail.com>
2024-03-07 13:04:01 +0100

    add card & transaction section in desktop layout

--- lib/interface/views/dashboard_desktop_layout.dart
+++ lib/interface/views/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/all_expenses_and_quick_invoice_section.dart';
+import '../widgets/card_and_transaction_section.dart';
 import '../widgets/drawer/custom_drawer.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -12,10 +13,9 @@ class DashboardDesktopLayout extends StatelessWidget {
       children: [
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
-        Expanded(
-          flex: 2,
-          child: AllExpensesAndQuickInvoiceSection(),
-        ),
+        Expanded(flex: 2, child: AllExpensesAndQuickInvoiceSection()),
+        SizedBox(width: 24),
+        Expanded(child: CardAndTransactionSection())
       ],
     );
   }
