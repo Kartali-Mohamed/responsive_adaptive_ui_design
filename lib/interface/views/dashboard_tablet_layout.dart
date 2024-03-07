e53a89ce71bb1fc13ef05644ff4f8d7c265e02d4
kartali mohamed <mohameekartali@gmail.com>
2024-03-07 17:01:37 +0100

    handle design of dashboad tablet layout

--- lib/interface/views/dashboard_tablet_layout.dart
+++ lib/interface/views/dashboard_tablet_layout.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_adaptive_ui_design/interface/views/dashboard_mobile_layout.dart';
+
+import '../widgets/drawer/custom_drawer.dart';
 
 class DashboardTabletLayout extends StatelessWidget {
   const DashboardTabletLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Row(
+      children: [
+        Expanded(child: CustomDrawer()),
+        SizedBox(width: 32),
+        Expanded(
+            flex: 3,
+            child: Padding(
+              padding: EdgeInsets.only(top: 40),
+              child: DashboardMobileLayout(),
+            )),
+        SizedBox(width: 24),
+      ],
+    );
   }
 }
