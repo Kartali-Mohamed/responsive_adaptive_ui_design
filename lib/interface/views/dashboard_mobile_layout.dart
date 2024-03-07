3aa49d8751b2c16be6c0c5e3d45c987b20d884c4
kartali mohamed <mohameekartali@gmail.com>
2024-03-07 17:01:46 +0100

    handle design of dashboad mobile layout

--- lib/interface/views/dashboard_mobile_layout.dart
+++ lib/interface/views/dashboard_mobile_layout.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_adaptive_ui_design/interface/widgets/all_expenses_and_quick_invoice_section.dart';
+
+import '../widgets/card_and_transaction_section.dart';
+import '../widgets/income/income_section.dart';
 
 class DashboardMobileLayout extends StatelessWidget {
   const DashboardMobileLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          AllExpensesAndQuickInvoiceSection(),
+          CardAndTransactionSection(),
+          SizedBox(height: 24),
+          IncomeSection(),
+        ],
+      ),
+    );
   }
 }
