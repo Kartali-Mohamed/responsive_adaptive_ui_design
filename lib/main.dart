e2ac56ba8aac015a59a155f73430dbc29725c233
Kartali-Mohamed <mohameekartali@gmail.com>
2024-03-10 15:50:20 +0100

    test ui with device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 import 'interface/views/dashboard_view.dart';
 
 void main() {
-  runApp(const ResponsiveDashboard());
+  runApp(DevicePreview(
+      enabled: true, builder: (context) => const ResponsiveDashboard()));
 }
 
 class ResponsiveDashboard extends StatelessWidget {
